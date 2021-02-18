86d72223e57e679350bb01a15b4a49afdcc208db
Cory Isaak <laughing.panjandrum@gmail.com>
2021-02-18 10:34:22 -0800

    Tweaked flask healing bonuses.

--- VileColossus/game.cpp
+++ VileColossus/game.cpp
@@ -9,7 +9,6 @@ TODO
 	reduce amt of blue material gained?? or give it some kind of sink?
 
 	halt quickmoving if we see an enemy
-	boost stats of higher-tier flasks
 	allow 'v' to view from equipment list and pickup menus
 	first blue enchantment costs more than the second??!!
 	add 'spellpower buff' flask effect
